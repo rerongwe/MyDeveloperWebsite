b54a046e6c8862bfbf023d3eb5b05da248a17dc7
Fernando Rerong <rerongwe@hotmail.com>
2025-12-10 20:32:13 -0300

    footer-changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 
@@ -43,6 +44,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     AppComponent,
     HeaderComponent,
     NavComponent,
+    FooterComponent,
     HomeComponent,
     NovidadesComponent,
     RoupasComponent,
